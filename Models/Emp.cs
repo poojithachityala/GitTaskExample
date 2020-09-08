cc02c1ef3056e7ced9ce49ad05980e47b0d3b728
poojithachityala <poojithareddy1031@gmail.com>
2020-09-08 12:07:50 +0530

    after adding models and controllers

---
+++ Models/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GitTaskEx.Models
+{[Table("Emp")]
+    public class Emp
+    {[Key]
+        public int EId { get; set; }
+        [Required]
+        public string EName { get; set; }
+        [Required]
+        public string EDesig { get; set; }
+        [Required]
+        public DateTime EDoj { get; set; }
+    }
+}
---
+++ Models/EmpContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GitTaskEx.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext() : base("Connection")
+        {
+
+        }
+        public DbSet<Emp> Emps { get; set; }
+    }
+}
